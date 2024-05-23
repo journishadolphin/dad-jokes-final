7c5d682c4e524fc384303e8a55baff7d9a87e69a
Journisha Dolphin <journishadolphin@uagateway.org>
2024-05-23 16:18:54 -0400

    Add main server file

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const server = require('./api/server.js');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3300;
+const authRouter = require('./api/auth/auth-router');
+const jokesRouter = require('./api/jokes/jokes-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/jokes', jokesRouter);
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log();
 });
